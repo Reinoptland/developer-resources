8259b4386dbaf58306688a1a419a678488ecbe5b
Reinoptland <reinoptland@gmail.com>
2020-03-11 16:06:03 +0100

    Handle action in reducer

--- src/store/developers/reducer.js
+++ src/store/developers/reducer.js
@@ -21,6 +21,9 @@ const initialState = [
 
 export default function(state = initialState, action) {
   switch (action.type) {
+    case "DELETE_DEVELOPER":
+      // console.log(state, action.payload, typeof action.payload);
+      return state.filter(developer => developer.id !== action.payload);
     case "ADD_DEVELOPER":
       return [...state, action.payload];
     default:
