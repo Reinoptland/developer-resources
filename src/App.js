ef13770bab0d600247ea6ca86b792848e60759e5
Reinoptland <reinoptland@gmail.com>
2020-03-11 14:16:15 +0100

    Add Developer Form Component

--- src/App.js
+++ src/App.js
@@ -5,15 +5,17 @@ import DeveloperList from "./components/DeveloperList";
 import ResourceList from "./components/ResourceList";
 import DeveloperDetails from "./components/DeveloperDetails";
 import ResourceDetails from "./components/ResourceDetails";
+import DeveloperForm from "./components/DeveloperForm";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <ResourceDetails />
-        {/* <DeveloperDetails />
-        <ResourceList />
         <DeveloperList />
+        <DeveloperForm />
+        {/* <ResourceDetails />
+        <DeveloperDetails />
+        <ResourceList />
         <ResourcesListByTag /> */}
       </header>
     </div>
---
+++ src/components/DeveloperForm.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function DeveloperForm() {
+  return (
+    <div>
+      <h1>ADD DEV</h1>
+      <form>
+        <label>Name</label>
+        <input />
+        <label>Website Url</label>
+        <input />
+      </form>
+    </div>
+  );
+}
